de0ec65dd4464b2e63b0a4b60e8c4cd32d24e5d6
Munkh-Orgil <munkhorgil@live.com>
2020-08-10 21:32:42 +0800

    add default options for redis init

--- src/index.ts
+++ src/index.ts
@@ -17,13 +17,24 @@ interface IOptions {
 export let client;
 
 export const init = (options?: IOptions) => {
-
   const { redisOptions } = options;
 
   if (redisOptions.host) {
     client = redisClient;
 
-    redisClient.init(redis.createClient(redisOptions));
+    const retryStrategy = (retryOptions) => Math.max(retryOptions.attempt * 100, 3000);
+
+    const options = {
+      host: redisOptions.host,
+      port: redisOptions.port,
+      password: redisOptions.password,
+      connect_timeout: redisOptions.timeout || 15000,
+      enable_offline_queue: redisOptions.enableOfflineQueue || true,
+      retryUnfulfilledCommands: redisOptions.enableOfflineQueue || true,
+      retry_strategy: redisOptions.retryStrategy || retryStrategy
+    };
+
+    redisClient.init(redis.createClient(options));
   } else {
     client = memoryClient;
   }
